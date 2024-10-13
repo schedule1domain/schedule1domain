78356c140d3bf3017de004a5ad4e790d7fdb4722
seancolinc <157671546+seancolinc@users.noreply.github.com>
2024-10-13 10:55:22 -0400

    Delete log_ip.php

--- log_ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ip = $_SERVER['REMOTE_ADDR']; // Get the visitor's IP address
-$date = date('Y-m-d H:i:s'); // Get the current date and time
-
-// Open the log file to append data
-$file = fopen("ip_log.txt", "a");
-fwrite($file, "IP: $ip - Date: $date\n");
-fclose($file);
-?>
